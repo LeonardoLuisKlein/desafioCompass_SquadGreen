c48cf794b521e7a7d867f82ad70cebc9a9ed36db
Jao <joao.henz.pb@compasso.com.br>
2022-07-06 15:14:36 -0300

    Feat: Checkbox

---
+++ src/components/micro/Checkbox/Checkbox.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input type="checkbox" id="checkbox" :v-model="text">
+        <label for="checkbox">{{ text }}</label>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "CheckboxComponent",
+    props: {
+        text: {
+            type: String
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import './Checkbox.scss'
+
+</style>
