8ab4cd06cf820ca454dd0f845f0f85e2ea3cb86b
Leonardo-Luis-Klein <leonardo.klein.pb@compasso.com.br>
2022-07-07 13:49:02 -0300

    Feat: Disable select texts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view to = "/"></router-view>
+    <router-view to="/"></router-view>
   </div>
 </template>
 
@@ -8,7 +8,13 @@
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
 * {
-  font-family: "Inter", "Nunito" ,sans-serif;
+  font-family: "Inter", "Nunito", sans-serif;
 }
 
+.disable-select {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
 </style>
