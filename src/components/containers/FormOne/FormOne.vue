4a6e230c39dafc6cfc0469f360e324f0d8a54fa1
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-07-07 11:08:58 -0300

    Feat: Created first form page

---
+++ src/components/containers/FormOne/FormOne.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <Header />
+    <div id="formOne">
+      <div class="sectionForm">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Header from '../../experience-fragments/Header/Header.vue'
+
+export default {
+  name: "formOne",
+  components: { Header }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './FormOne.scss';
+</style>
