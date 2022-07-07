95fda7dd0fdbacd6824cdb8cd3c413303ca52131
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-07-07 15:35:34 -0300

    Feat: Created Button Component

---
+++ src/components/micro/Button/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button :class="classButton" v-on:click="clickButton">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: "ButtonComponent",
+    props: {
+        text: {
+            type: String,
+            default: ""
+        },
+        classButton: {
+            type: String
+        },
+        clickButton: {
+            type: String,
+            default: ""
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import './Button.scss';
+</style>
