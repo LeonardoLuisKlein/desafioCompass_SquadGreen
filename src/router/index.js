a79771a880af8f401637d6c7d1bb7a8199310fe4
Leonardo-Luis-Klein <leonardo.klein.pb@compasso.com.br>
2022-07-08 15:15:09 -0300

    Feat: Adding error 401 route

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import FormOne from "@/components/containers/FormOne/FormOne.vue"
 import FormSecond from '@/components/containers/FormSecond/FormSecond.vue'
 import Error404 from '@/components/routesComponents/error404/Error404.vue'
+import Error401 from '@/components/routesComponents/error401/Error401.vue'
 
 Vue.use(VueRouter)
 
@@ -19,8 +20,13 @@ const routes = [
   },
   {
     path: "*",
-    name: "Error 404",
+    name: "Error404",
     component: Error404,
+  },
+  {
+    path: "/error401",
+    name: "Error401",
+    component: Error401,
   },
  
 ]
