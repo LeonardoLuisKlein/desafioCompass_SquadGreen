771d121daa38978836f8e7fb88cc706f731fcc61
Leonardo-Luis-Klein <leonardo.klein.pb@compasso.com.br>
2022-07-08 14:24:52 -0300

    Feat: Error 404 html

---
+++ src/components/routesComponents/error404/Error404.vue
@@ -0,0 +1,14 @@
+<template>
+  <img src="@/assets/gif404.gif" alt="Erro 404" class="errorgif">
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line
+    name: "Error404"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
