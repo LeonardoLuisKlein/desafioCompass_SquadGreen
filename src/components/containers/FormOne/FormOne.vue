5216b586c18bd4585c5e80fa1aa7b9212ac2ace5
Jao <joao.henz.pb@compasso.com.br>
2022-07-07 21:42:49 -0300

    Feat: Body Form and improvements on input/label components

--- src/components/containers/FormOne/FormOne.vue
+++ src/components/containers/FormOne/FormOne.vue
@@ -11,6 +11,44 @@
             <Button text="Social" classButton="Border"/>
             <Button text="Certificates" classButton="Border"/>
         </div>
+        <div class="divBody"> 
+          <div class="nameBody">
+            <Label inputLabel="name" className="defaultLabel" text="Full Name *"/>
+            <Inputs inputClass="inputBorder bigInput" id="name" placeHolder="Foo Bar"/>
+          </div>
+
+          <div class="nickNameBody">
+            <Label inputLabel="nickName" className="defaultLabel" text="NickName *"/>
+            <Inputs inputClass="inputBorder bigInput" id="nickName" placeHolder="Juanito"/>
+          </div>
+
+          <div class="emailPhoneBody">
+            <div class="emailBody">
+              <Label inputLabel="email" className="defaultLabel emailLabel" text="Email *"/>
+              <Inputs inputClass="inputBorder bigInput" id="email" placeHolder="foo@bar.com"/>
+            </div>
+
+            <div class="phoneBody">
+              <Label inputLabel="phone" className="defaultLabel phoneLabel" text="Phone *"/>
+              <Inputs inputClass="inputBorder bigInput" id="phone" placeHolder="(83) 00000-0000"/>
+            </div>
+          </div>
+
+          <div class="datesBody">
+            <div class="datesBodyHeader">
+              <Label inputLabel="" className="defaultLabel phoneLabel" text="Birthday *"/>
+            </div>
+
+            <div class="datesDay">
+              <Label inputLabel="day" className="defaultLabel dateLabel" text="Day"/>
+            </div>
+
+          </div>
+            
+          
+
+
+        </div>
       </div>
     </div>
   </div>
@@ -21,10 +59,12 @@
 import Header from '../../experience-fragments/Header/Header.vue'
 import Title from '@/components/micro/Title/Title.vue'
 import Button from '../../micro/Button/Button.vue'
+import Label from '@/components/micro/Label/Label.vue'
+import Inputs from '@/components/micro/Inputs/Inputs.vue'
 
 export default {
   name: "formOne",
-  components: { Header, Title, Button }
+  components: { Header, Title, Button, Label, Inputs }
 }
 </script>
 
