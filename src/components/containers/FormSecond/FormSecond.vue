9867c61f91f2a7e7d0a42f0d6e4339b7b2999566
Leonardo-Luis-Klein <leonardo.klein.pb@compasso.com.br>
2022-07-11 15:36:47 -0300

    Style: Display flex on error message

--- src/components/containers/FormSecond/FormSecond.vue
+++ src/components/containers/FormSecond/FormSecond.vue
@@ -1,36 +1,39 @@
 <template>
 
-    <div class="header">
+  <div class="header">
     <Header />
     <div id="FormSecond">
       <div class="sectionForm">
         <div class="divTitle">
-            <Title type="h1" tMessage="Team sign Up" nameClass="titleDefault"/>
+          <Title type="h1" tMessage="Team sign Up" nameClass="titleDefault" />
         </div>
         <div class="buttonsTop">
-            <Button text="Basic" classButton="Border"/>
-            <Button text="Social" classButton="Border selected"/>
-            <Button text="Certificates" classButton="Border"/>
+          <Button text="Basic" classButton="Border" />
+          <Button text="Social" classButton="Border selected" />
+          <Button text="Certificates" classButton="Border" />
         </div>
-        <div class="divBody"> 
+        <div class="divBody">
           <div class="linkedinBody">
-            <Label inputLabel="linkedin" className="defaultLabel" text="Linkedin"/>
-            <Inputs inputClass="inputBorder bigInput" id="linkedin" placeHolder="https://www.linkedin.com/in/foo-bar-3a0560104/"/>
+            <Label inputLabel="linkedin" className="defaultLabel" text="Linkedin" />
+            <Inputs inputClass="inputBorder bigInput" id="linkedin"
+              placeHolder="https://www.linkedin.com/in/foo-bar-3a0560104/" />
           </div>
 
           <div class="githubBody">
-            <Label inputLabel="github" className="defaultLabel" text="Github *"/>
+            <div class="separationName">
+            <Label inputLabel="github" className="defaultLabel" text="Github *" />
             <TextComponent text="Please enter your GitHub Link" className="mediumLowSize error" />
-            <Inputs inputClass="inputBorder bigInput" id="gihub" placeHolder="https://github.com/foobar"/>
           </div>
+          <Inputs inputClass="inputBorder bigInput" id="gihub" placeHolder="https://github.com/foobar" />
         </div>
-        
-        <div class="divFooter">
-          <Button text="Next >" classButton="next"/>
-        </div>
-
       </div>
+
+      <div class="divFooter">
+        <Button text="Next >" classButton="next" />
+      </div>
+
     </div>
+  </div>
   </div>
 
 </template>
@@ -44,16 +47,14 @@ import Label from '@/components/micro/Label/Label.vue'
 import Inputs from '@/components/micro/Inputs/Inputs.vue'
 import TextComponent from '@/components/micro/Text/Text.vue'
 export default {
-    name: "FormSecond",
-    components: {
-        Header, Title, Button, Label, Inputs, TextComponent,
-    }
+  name: "FormSecond",
+  components: {
+    Header, Title, Button, Label, Inputs, TextComponent,
+  }
 }
 
 </script>
 
 <style lang="scss" scoped>
-
 @import './FormSecond.scss'
-
 </style>
