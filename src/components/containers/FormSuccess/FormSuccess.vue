12d27abefa7157371f08ad03c20e4c1b387f6741
Jao <joao.henz.pb@compasso.com.br>
2022-07-12 15:16:38 -0300

    Feat: formSuccess responsivity

--- src/components/containers/FormSuccess/FormSuccess.vue
+++ src/components/containers/FormSuccess/FormSuccess.vue
@@ -4,25 +4,28 @@
     <div id="formSuccess">
       <div class="sectionForm">
         
-        <div class="formBody">
-            <TextComponent text="Your data has been sent successfully!"/> 
-            <TextComponent text="Full Name: "/> 
-            <TextComponent text="Email: "/> 
-            <TextComponent text="Birthday: "/> 
-            <TextComponent text="Age: "/> 
-            <TextComponent text="GitHub: "/> 
-            <TextComponent text="Certificates: "/> 
-            <TextComponent text="Team Name: "/> 
-            <TextComponent text="Institution: "/> 
-            <TextComponent text="Graduation: "/>
 
-            
-        </div> 
+
+          <div class="formBody">
+              <TextComponent text="Your data has been sent successfully!"/> 
+              <TextComponent text="Full Name: "/> 
+              <TextComponent text="Email: "/> 
+              <TextComponent text="Birthday: "/> 
+              <TextComponent text="Age: "/> 
+              <TextComponent text="GitHub: "/> 
+              <TextComponent text="Certificates: "/> 
+              <TextComponent text="Team Name: "/> 
+              <TextComponent text="Institution: "/> 
+              <TextComponent text="Graduation: "/>
+
+              
+          </div> 
         
 
-        <div class="formButton">
-            <Button text="Return" classButton="next"/>
-        </div>
+          <div class="formButton">
+              <Button text="Return" classButton="next"/>
+          </div>
+
 
         
         
