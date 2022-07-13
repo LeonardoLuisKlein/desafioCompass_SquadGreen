a8c8af3b787048598b58744a08c57518c9c3f297
Leonardo-Luis-Klein <leonardo.klein.pb@compasso.com.br>
2022-07-13 15:19:28 -0300

    Feat: Adjusting RegEx and local storage

--- src/store/index.js
+++ src/store/index.js
@@ -5,21 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    github: ""
+    github: "",
+    control: "",
   },
   getters: {
   },
   mutations: {
     setGitHub(state, github) {
       state.github = github;
-      if (/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(github)) {
-        window.localStorage.setItem("github", github);
-      } else {
-        window.localStorage.setItem("github", "");
+      if(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(github)){
+      localStorage.setItem("github", github);
+      this.state.control = true;
+      } else{
+        localStorage.setItem("github", "");
+        this.state.control = false;
       }
     },
   },
   actions: {
+    setGitHub({ commit }, github) {
+      commit("setGitHub", github);
+    },
   },
   modules: {
   }
