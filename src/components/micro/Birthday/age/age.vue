95a13f1b3517d51a46ebc7689562518a041bee13
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-07-14 16:23:08 -0300

    Feat: Created Component Birthday

---
+++ src/components/micro/Birthday/age/age.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Label inputLabel="age" className="defaultLabel dateLabel" text="Age" />
+    <div  class="age">
+      <h1 contentEditable=false data-ph="18" id="ageId" class="resultAge">{{ $store.state.age }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import Label from '../../Label/Label.vue'
+export default {
+  // eslint-disable-next-line
+  name: "Age",
+  components: { Label },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./age.scss";
+</style>
---
+++ src/components/micro/Birthday/month/month.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <Label inputLabel="month" className="defaultLabel dateLabel" text="Month" />
+        <select v-model="monthSelect" class="month" @change="valueMonth">
+            <option disabled value="01">Month</option>
+            <option disabled value="" style="display:none">1</option>
+            <option id="optionMonth" v-for="option in options" v-bind:key="option.month">{{ option.month }}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import Label from '../../Label/Label.vue'
+export default {
+    // eslint-disable-next-line
+    name: "Month",
+    data() {
+        return {
+            monthSelect: "1",
+            options: [
+                { month: 1 },
+                { month: 2 },
+                { month: 3 },
+                { month: 4 },
+                { month: 5 },
+                { month: 6 },
+                { month: 7 },
+                { month: 8 },
+                { month: 9 },
+                { month: 10 },
+                { month: 11 },
+                { month: 12 },
+            ]
+        };
+    },
+    components: { Label },
+    methods: {
+        ...mapActions(["setMonth"]),
+        valueMonth(e) {
+            this.setMonth(e.target.value)
+        },
+    },
+}
+</script>
+<style lang="scss" scoped>
+@import "./month.scss";
+</style>
