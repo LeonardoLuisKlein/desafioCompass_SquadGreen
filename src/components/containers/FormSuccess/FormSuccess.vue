7eae7b974ec30635401d04888630ba81c9892bac
Leonardo-Luis-Klein <leonardo.klein.pb@compasso.com.br>
2022-07-15 14:33:30 -0300

    Feat: Catching local storage values and putting on success tab

--- src/components/containers/FormSuccess/FormSuccess.vue
+++ src/components/containers/FormSuccess/FormSuccess.vue
@@ -8,28 +8,23 @@
 
           <div class="formBody">
               <TextComponent text="Your data has been sent successfully!"/> 
-              <TextComponent text="Full Name: "/> 
-              <TextComponent text="Email: "/> 
-              <TextComponent text="Birthday: "/> 
-              <TextComponent text="Age: "/> 
-              <TextComponent text="GitHub: "/> 
+              <TextComponent text="Full Name:" :cText="this.fullname"/> 
+              <TextComponent text="Email: " :cText="this.email"/> 
+              <TextComponent text="Birthday: " /> 
+              <TextComponent text="Age: " :cText="this.age"/> 
+              <TextComponent text='GitHub: ' :cText="this.github"/> 
               <TextComponent text="Certificates: "/> 
-              <TextComponent text="Team Name: "/> 
-              <TextComponent text="Institution: "/> 
-              <TextComponent text="Graduation: "/>
+              <TextComponent text="Team Name: " :cText="this.teamName"/> 
+              <TextComponent text="Institution: " :cText="this.institution"/> 
+              <TextComponent text="Graduation: " :cText="this.graduation"/>
 
               
           </div> 
         
 
           <div class="formButton">
-              <Button text="Return" classButton="next"/>
+              <Button text="Return" classButton="next" :clickButton="clearLocalStorage"/>
           </div>
-
-
-        
-        
-
       </div>
     </div>
   </div>
@@ -46,7 +41,41 @@ export default {
         Header,
         TextComponent,
         Button
-    }
+    },
+    data(){
+        return{
+            fullname: "",
+            email: "",
+            birthday: "",
+            age: "",
+            github: "",
+            certificates: "",
+            teamName: "",
+            institution: "",
+            graduation: "",
+        }
+    },
+
+    methods:{
+        getLocalStorage(){
+            this.fullname = localStorage.getItem("fullname")
+            this.email = localStorage.getItem("email")
+            this.age = localStorage.getItem("age")
+            this.github = localStorage.getItem("github")
+            this.teamName = localStorage.getItem("teamName")
+            this.institution = localStorage.getItem("institution")
+            this.graduation = localStorage.getItem("graduation")
+        },
+
+        clearLocalStorage(){
+            localStorage.clear()
+            this.$router.push('/')
+        }
+    },
+
+      mounted() {
+    this.getLocalStorage();
+  },
 }
 
 </script>
--- src/components/experience-fragments/Header/Header.vue
+++ src/components/experience-fragments/Header/Header.vue
@@ -2,7 +2,7 @@
   <div id="divHeader">
     <section class="sectionTitle">
       <Title v-if="this.$route" type="h1" tMessage="Forms" class="smallTitle" />
-      <Title v-if="this.$route.name === 'formOne'" type="h2" tMessage="First Tab" class="blueTitle" />
+      <Title v-if="this.$route.name === 'FormOne'" type="h2" tMessage="First Tab" class="blueTitle" />
       <Title v-if="this.$route.name === 'FormSecond'" type="h2" tMessage="Second Tab" class="blueTitle" />
       <Title v-if="this.$route.name === 'FormThird'" type="h2" tMessage="Third Tab" class="blueTitle" />
       <Title v-if="this.$route.name === 'FormSuccess'" type="h2" tMessage="Success Tab" class="blueTitle" />
